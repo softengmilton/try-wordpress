44fde1961031610ce63fd8e93e0a0fb02270d68e
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-08-22 21:20:40 +0600

    play14 Modular Admnistration Page

--- wp-content/plugins/alicade/inc/Pages/Admin.php
+++ wp-content/plugins/alicade/inc/Pages/Admin.php
@@ -3,22 +3,34 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {
+    public $settings;
+    public $pages = array();
+
+    public function __construct()
+    {
+        $this->settings = new SettingsApi;
+
+        $this->pages = array(
+            array(
+                'page_title' => 'Alicade Plugin',
+                'menu_title' => 'Alicade',
+                'capability' => 'manage_options',
+                'menu_slug'  => 'alicade',
+                'callback'   => function () {
+                    echo '<h1>Plugin</h1>';
+                },
+                'icon_url'   => 'dashicons-store',
+                'position'   => 110,
+            ),
+        );
+    }
+
     public function register()
     {
-        add_action('admin_menu', array($this, 'add_admin_pages'));
-    }
-
-    public function add_admin_pages()
-    {
-        add_menu_page('Alicade', 'Alicad', 'manage_options', 'alicade', array($this, 'admin_index'), 'dashicons-store', 110);
-    }
-
-    public function admin_index()
-    {
-        // require template 
-        require_once $this->plugin_path . 'templates/admin.php';
+        $this->settings->AddPages($this->pages)->register();
     }
 }
---
+++ wp-content/plugins/alicade/readme.md
@@ -0,0 +1,14 @@
+# plugin 101 series
+
+Full list of sections and features we will build during the series of Tutorials
+
+*Modular Administration Area
+* CPT Manager
+* Taxonomy Manager
+* Widget to upload and display media in sidebar
+* Post and Pages Gallery integration
+* Testimonial section: Comment in the frontend, Admmins can approve Comments, select which comments to display 
+* Custom template sections
+* Ajax based Login/Register system
+* Membership protected Area
+* Chat system
