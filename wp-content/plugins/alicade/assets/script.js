38bf9363969952450236d489c7320d38de71a142
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-08-24 22:07:54 +0600

    play18 Admin tabs in vanilla js

--- wp-content/plugins/alicade/assets/script.js
+++ wp-content/plugins/alicade/assets/script.js
@@ -1,1 +1,20 @@
-  
+window.addEventListener("load", function() {
+    let tabs = document.querySelectorAll(".nav-tabs li");
+    let contents = document.querySelectorAll(".tab-content > div");
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", function() {
+            tabs.forEach(t => t.classList.remove("active"));
+            contents.forEach(c => c.style.display = "none");
+
+            tab.classList.add("active");
+            contents[index].style.display = "block";
+        });
+    });
+
+    // Show the first tab by default
+    if (tabs.length > 0) {
+        tabs[0].classList.add("active");
+        contents[0].style.display = "block";
+    }
+}); 
--- wp-content/plugins/alicade/templates/admin.php
+++ wp-content/plugins/alicade/templates/admin.php
@@ -3,13 +3,30 @@
     <?php settings_errors(); ?>
 
 
-    <form action="options.php" method="post">
-        <?php
+    <div class="nav nav-tabs">
+        <li class="active"><a href="#tab-1">Manage Settings</a></li>
+        <li><a href="#tab-2">Updates</a></li>
+        <li><a href="#tab-3">About</a></li>
+    </div>
 
-        settings_fields('alicad_options_group');
-        do_settings_sections('alicade');
-        submit_button();
+    <div class="tab-content">
+        <div id="tab-1" class="tab-pane active">
+            <form action="options.php" method="post">
+                <?php
 
-        ?>
-    </form>
+                settings_fields('alicad_options_group');
+                do_settings_sections('alicade');
+                submit_button();
+
+                ?>
+            </form>
+
+        </div>
+        <div id="tab-2" class="tab-pane">
+            <h3>Updates</h3>
+        </div>
+        <div id="tab-3" class="tab-pane">
+            <h3>About</h3>
+        </div>
+    </div>
 </div>
