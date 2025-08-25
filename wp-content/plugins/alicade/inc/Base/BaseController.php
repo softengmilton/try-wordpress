3a26dd78a35071e27ccab1ec9c3cc00bac3b768d
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-08-26 04:17:37 +0600

    play21 code cleanup

--- wp-content/plugins/alicade/inc/Base/BaseController.php
+++ wp-content/plugins/alicade/inc/Base/BaseController.php
@@ -12,11 +12,24 @@ class BaseController
     public $plugin_path;
     public $plugin_url;
     public $plugin;
+    public $managers = array();
 
     public function __construct()
     {
         $this->plugin_path = plugin_dir_path(dirname(__FILE__, 2));
         $this->plugin_url = plugin_dir_url(dirname(__FILE__, 2));
         $this->plugin = plugin_basename(dirname(__FILE__, 3)) . '/alicade.php';
+
+        $this->managers = array(
+            'cpt_manager'        => 'CPT Manager',
+            'taxonomy_manager'   => 'Taxonomy Manager',
+            'media_widget'       => 'Media Widget',
+            'gallery_manager'    => 'Gallery Manager',
+            'testimonial_manager' => 'Testimonial Manager',
+            'templates_manager'  => 'Templates Manager',
+            'login_manager'      => 'Login Manager',
+            'membership_manager' => 'Membership Manager',
+            'chat_manager'       => 'Chat Manager',
+        );
     }
 }
--- wp-content/plugins/alicade/inc/Pages/Admin.php
+++ wp-content/plugins/alicade/inc/Pages/Admin.php
@@ -83,78 +83,25 @@ class Admin extends BaseController
             ),
         );
     }
-
     public function setSettings()
     {
-        $args = array(
-            // array(
-            //     'option_group' => 'alicad_options_group',
-            //     'option_name'  => 'text_example',
-            //     'callback'     => array($this->callbacks, 'alicadOptionsGroup')
-            // ),
-            // array(
-            //     'option_group' => 'alicad_options_group',
-            //     'option_name'  => 'first_name',
-            // ),
-            array(
+        $args = array();
+        foreach ($this->managers as $key => $value) {
+            $args[] = array(
                 'option_group' => 'alicad_plugin_settings',
-                'option_name'  => 'cpt_manager',
+                'option_name'  => $key,
                 'callback'     => array($this->callbacks, 'checkboxSanitize')
-            ),
-            array(
-                'option_group' => 'alicad_plugin_settings',
-                'option_name'  => 'taxonomy_manager',
-                'callback'     => array($this->callbacks, 'checkboxSanitize')
-            ),
-            array(
-                'option_group' => 'alicad_plugin_settings',
-                'option_name'  => 'media_widget',
-                'callback'     => array($this->callbacks, 'checkboxSanitize')
-            ),
-            array(
-                'option_group' => 'alicad_plugin_settings',
-                'option_name'  => 'gallery_manager',
-                'callback'     => array($this->callbacks, 'checkboxSanitize')
-            ),
-            array(
-                'option_group' => 'alicad_plugin_settings',
-                'option_name'  => 'testimonial_manager',
-                'callback'     => array($this->callbacks, 'checkboxSanitize')
-            ),
-            array(
-                'option_group' => 'alicad_plugin_settings',
-                'option_name'  => 'templates_manager',
-                'callback'     => array($this->callbacks, 'checkboxSanitize')
-            ),
-            array(
-                'option_group' => 'alicad_plugin_settings',
-                'option_name'  => 'login_manager',
-                'callback'     => array($this->callbacks, 'checkboxSanitize')
-            ),
-            array(
-                'option_group' => 'alicad_plugin_settings',
-                'option_name'  => 'membership_manager',
-                'callback'     => array($this->callbacks, 'checkboxSanitize')
-            ),
-            array(
-                'option_group' => 'alicad_plugin_settings',
-                'option_name'  => 'chat_manager',
-                'callback'     => array($this->callbacks, 'checkboxSanitize')
-            ),
-        );
+            );
+        }
 
         $this->settings->setSettings($args);
     }
 
+
+
     public function setSections()
     {
         $args = array(
-            // array(
-            //     'id'        => 'alicade_admin_index',
-            //     'title'     => 'Settings',
-            //     'callback'  => array($this->callbacks, 'alicadAdminSection'),
-            //     'page'      =>  'alicade',
-            // ),
             array(
                 'id'        => 'alicade_admin_index',
                 'title'     => 'Settings Manger',
@@ -166,152 +113,26 @@ class Admin extends BaseController
         $this->settings->setSections($args);
     }
 
+
     public function setFields()
     {
-        $args = array(
-            // array(
-            //     'id'        => 'text_example',
-            //     'title'     => 'Text Example',
-            //     'callback'  => array($this->callbacks, 'alicadTextExample'),
-            //     'page'      =>  'alicade',
-            //     'section'   => 'alicade_admin_index',
-            //     'args'      => array(
-            //         'label_for' => 'text_example',
-            //         'class'     => 'example-class',
+        $args = array();
 
-            //     )
-            // ),
-            // array(
-            //     'id'        => 'first_name',
-            //     'title'     => 'First Name',
-            //     'callback'  => array($this->callbacks, 'alicadFirstName'),
-            //     'page'      =>  'alicade',
-            //     'section'   => 'alicade_admin_index',
-            //     'args'      => array(
-            //         'label_for' => 'text_example',
-            //         'class'     => 'example-class',
-
-            //     )
-            // ),
-            array(
-                'id'        => 'cpt_manager',
-                'title'     => 'Activate CPT Manager',
+        foreach ($this->managers as $key => $value) {
+            $args[] = array(
+                'id'        => $key,
+                'title'     => 'Activate ' . $value,
                 'callback'  => array($this->callbacks_mngr, 'checkboxField'),
-                'page'      =>  'alicade',
+                'page'      => 'alicade',
                 'section'   => 'alicade_admin_index',
                 'args'      => array(
-                    'option_name' => 'alecaddd_plugin',
-                    'label_for' => 'cpt_manager',
-                    'class'     => 'ui-toggle'
-
+                    'option_name' => 'alicade_plugin', // consistent with setSettings
+                    'label_for'   => $key,
+                    'class'       => 'ui-toggle'
                 )
-            ),
-            array(
-                'id'        => 'taxonomy_manager',
-                'title'     => 'Activate Taxonomy Manager',
-                'callback'  => array($this->callbacks_mngr, 'checkboxField'),
-                'page'      =>  'alicade',
-                'section'   => 'alicade_admin_index',
-                'args'      => array(
-                    'option_name' => 'alecaddd_plugin',
-                    'label_for' => 'taxonomy_manager',
-                    'class'     => 'ui-toggle'
+            );
+        }
 
-                )
-            ),
-            array(
-                'id'        => 'media_widget',
-                'title'     => 'Activate Widget Manager',
-                'callback'  => array($this->callbacks_mngr, 'checkboxField'),
-                'page'      =>  'alicade',
-                'section'   => 'alicade_admin_index',
-                'args'      => array(
-                    'option_name' => 'alecaddd_plugin',
-                    'label_for' => 'media_widget',
-                    'class'     => 'ui-toggle'
-
-                )
-            ),
-            array(
-                'id'        => 'gallery_manager',
-                'title'     => 'Activate Gallery Manager',
-                'callback'  => array($this->callbacks_mngr, 'checkboxField'),
-                'page'      =>  'alicade',
-                'section'   => 'alicade_admin_index',
-                'args'      => array(
-                    'option_name' => 'alecaddd_plugin',
-                    'label_for' => 'gallery_manager',
-                    'class'     => 'ui-toggle'
-
-                )
-            ),
-            array(
-                'id'        => 'testimonial_manager',
-                'title'     => 'Activate Testimonial Manager',
-                'callback'  => array($this->callbacks_mngr, 'checkboxField'),
-                'page'      =>  'alicade',
-                'section'   => 'alicade_admin_index',
-                'args'      => array(
-                    'option_name' => 'alecaddd_plugin',
-                    'label_for' => 'testimonial_manager',
-                    'class'     => 'ui-toggle'
-
-                )
-            ),
-            array(
-                'id'        => 'templates_manager',
-                'title'     => 'Activate Templates Manager',
-                'callback'  => array($this->callbacks_mngr, 'checkboxField'),
-                'page'      =>  'alicade',
-                'section'   => 'alicade_admin_index',
-                'args'      => array(
-                    'option_name' => 'alecaddd_plugin',
-                    'label_for' => 'templates_manager',
-                    'class'     => 'ui-toggle'
-
-                )
-            ),
-            array(
-                'id'        => 'login_manager',
-                'title'     => 'Activate login Manager',
-                'callback'  => array($this->callbacks_mngr, 'checkboxField'),
-                'page'      =>  'alicade',
-                'section'   => 'alicade_admin_index',
-                'args'      => array(
-                    'option_name' => 'alecaddd_plugin',
-                    'label_for' => 'login_manager',
-                    'class'     => 'ui-toggle'
-
-                )
-            ),
-            array(
-                'id'        => 'membership_manager',
-                'title'     => 'Activate Membership Manager',
-                'callback'  => array($this->callbacks_mngr, 'checkboxField'),
-                'page'      =>  'alicade',
-                'section'   => 'alicade_admin_index',
-                'args'      => array(
-                    'option_name' => 'alecaddd_plugin',
-                    'label_for' => 'membership_manager',
-                    'class'     => 'ui-toggle'
-
-                )
-            ),
-            array(
-                'id'        => 'chat_manager',
-                'title'     => 'Activate Chat Manager',
-                'callback'  => array($this->callbacks_mngr, 'checkboxField'),
-                'page'      =>  'alicade',
-                'section'   => 'alicade_admin_index',
-                'args'      => array(
-                    'option_name' => 'alecaddd_plugin',
-                    'label_for' => 'chat_manager',
-                    'class'     => 'ui-toggle'
-
-                )
-            ),
-        );
-
-        $this->settings->setFields($args);
+        $this->settings->setFields($args); // actually register fields
     }
 }
