969c645336aea4026708938a6ffc61de943d52c3
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-08-27 01:06:37 +0600

    play23 Save Default options on Activate

--- wp-content/plugins/alicade/inc/Base/Activate.php
+++ wp-content/plugins/alicade/inc/Base/Activate.php
@@ -11,5 +11,12 @@ class Activate
     public static function activate()
     {
         flush_rewrite_rules();
+
+        if (get_option('alicade')) {
+            return;
+        }
+
+        $default = array();
+        update_option('alicade', $default);
     }
 }
